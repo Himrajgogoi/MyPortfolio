ee0d450cf3e1ab213dae42e804aeeaf6a4fbb6fa
HIMRAJ GOGOI <galacticmacawesomeville@gmail.com>
2021-09-07 08:51:13 +0530

    background improvement

--- pages/index.js
+++ pages/index.js
@@ -295,7 +295,7 @@ export default function Home({ isConnected, skills, frameworks, databases, artic
               <div>
                 <h2 className="animate fadeInLeft left">Myself, Himraj Gogoi.</h2>
                 <h4 className="animate fadeInRight right">Web Developer, Mobile App Developer and a programming enthusiast.</h4>
-                <h5 className="animate fadeInDown down">Scroll below.</h5>
+                <h5 className="animate fadeInDown down" style={{color: `#ffde59`}}>Scroll below.</h5>
               </div>
           </div>
           <Modal isOpen={isOpen_skills}>
